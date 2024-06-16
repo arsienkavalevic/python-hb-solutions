86d9a663ef5864802c6682339526fbbbd37b1504
Arsien Kavalievic <arsienkavalievic@gmail.com>
2024-06-16 16:55:18 +0300

    Add 2.2A

---
+++ 2.2/A.py
@@ -0,0 +1,7 @@
+username = input("Как Вас зовут?\n")
+print(f"Здравствуйте, {username}!")
+answer = input("Как дела?\n")
+if answer == "хорошо":
+    print("Я за вас рада!")
+elif answer == "плохо":
+    print("Всё наладится!")
